a363bf30a5036c5a3d26127e0dcaf057b24e96e5
Maëva Arlandis <maeva.arlandis@gmail.com>
2018-11-14 22:09:22 +0100

    env shows food and enemies

--- agent.py
+++ agent.py
@@ -7,31 +7,38 @@ class Agent:
     """Robot class with:
     - position
     - energy level
+    TODO: sensors
 
     """
 
     def __init__(self): #constructor
 
-        self.coord = [13,19]
+        self.coord = [18,12]
         self.energy = 40
-
 
     def move(self, direction):
 
+        x=self.coord[0]
+        y=self.coord[1]
         if direction == "w": #move west
             x = x - 1
-            self.energy=-1
+            self.energy-=1
         elif direction == "e": #move east
             x = x + 1
-            self.energy=-1
+            self.energy-=1
         elif direction == "n": #move north
             y = y - 1
-            self.energy=-1
+            self.energy-=1
         elif direction == "s": #move south
             y = y + 1
-            self.energy=-1
+            self.energy-=1
         else:
             pass
+        self.coord=[x,y]
 
+    def eat(self):
+        energy=+15    #is there a max level ?
+
+          
     def __repr__(self):
         return "Position:{0}  Energy:{1}".format(self.coord,self.energy)
--- environment.py
+++ environment.py
@@ -2,6 +2,8 @@
 """This module contains the Environment class"""
 
 from agent import *
+import copy
+import random
 
 class Environment:
 
@@ -23,9 +25,9 @@ class Environment:
         self.size = 25
         self.agent = Agent()
         self.map = []
+        self.food_counter=0
         self.init_map()
-        self.food = self.init_food()
-        #self.enemies = self.init_enemies()
+        self.enemies = [[1,12],[6,12],[6,6],[6,18]]
 
     def init_map(self):
         """ Initialize a clean map of size 25x25 with just obstacles"""
@@ -34,19 +36,43 @@ class Environment:
         for i in range(len(lignes)):
             self.map.append(list(lignes[i])[:self.size])
         f.close()
+        self.init_food()    
 
-    """def init_food():
-        for i in range(15):
-            list.append
-
-    def is_free():
-        if map"""
-
+    def loaf_map(self, name):
+        f=open(name,'r')
+        lignes =f.readlines()
+        for i in range(len(lignes)):
+            self.map.append(list(lignes[i])[:self.size])
+        f.close()
+        
+    def init_food(self):
+        """ add food into the map
+        TODO: verif no food into the map at the begnning"""
+        while self.food_counter<15:
+            x=int(random.uniform(0,25))
+            y=int(random.uniform(0,25))
+            #print("x:{0}  y:{1}".format(x,y))
+            if self.map[x][y]==' ':
+                self.map[x][y]='$'
+                self.food_counter+=1
+                
     def show(self):
-        for i in range(len(self.map)):
+        c_map=copy.deepcopy(self.map[:])
+        c_map[self.agent.coord[0]][self.agent.coord[1]]="I"
+        for e in self.enemies:
+            c_map[e[0]][e[1]]="E"
+        #print(c_map)
+        print("---------------------------")
+        for i in range(len(c_map)):
             str=""
-            for c in self.map[i]:
+            for c in c_map[i]:
                 str=str+c
-            print(str)
+            print("|"+str+"|")
+        
+        print("---------------------------")
+        str2=""
+        for i in range(self.agent.energy):
+            str2=str2+"H"
+        print(str2)
                 
 
